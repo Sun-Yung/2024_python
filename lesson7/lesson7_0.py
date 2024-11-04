3a02ab5f83cd8142d7f6e1aa31bbd943e7a0ed70
Sun <ccps88207@gmail.com>
2024-11-04 13:47:08 +0800

    lesson7-2

--- lesson7/lesson7_0.py
+++ lesson7/lesson7_0.py
@@ -1,3 +1,4 @@
+
 import datasource
 
 from tkinter import ttk
@@ -72,14 +73,16 @@ class Window(ThemedTk):
     def sitename_selected(self,event):
         selected = self.selected_site.get()
         print(selected)
-        # selected_data = datasource.get_selected_data(selected)
-        # for record in selected_data:
-        #     self.tree.insert("", "end", values=record)
+        #selected_data = datasource.get_selected_data(selected)
+        #for record in selected_data:
+            #self.tree.insert("", "end", values=record)
 
     
         
+ 
 
 def main():
+    datasource.download_data() #下載至資料庫
     window = Window(theme="arc")
     window.mainloop()
 
