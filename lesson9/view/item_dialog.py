91c73cdb5e5a418d120a2708f01705c8ba72af4e
Sun <ccps88207@gmail.com>
2024-11-13 14:54:29 +0800

    modify

--- lesson9/view/item_dialog.py
+++ lesson9/view/item_dialog.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.simpledialog import Dialog
+from PIL import Image,ImageTk
 
 class MycustomDialog(Dialog):
     def __init__(self,parent,record:list,title=None):
@@ -17,20 +18,33 @@ class MycustomDialog(Dialog):
 
     def body(self, master):
         # 創建對話框主體。返回應具有初始焦點的控件。
-        main_frame=ttk.Frame(master,borderwidth=1,relief='groove')
-       
-        canvas_left=tk.Canvas(main_frame,width=200,height=200)
-        canvas_left.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
-        canvas_left.create_text(200,150,text="優",font=("Helvetica",24,"bold"),fill="blue")
-        canvas_left.pack(side="left")
+        main_frame = ttk.Frame(master,borderwidth=1,relief='groove')
+        canvas_left = tk.Canvas(main_frame,width=200,height=200)
+        canvas_left.create_rectangle(10,10,190,190,outline="#9E7A7A",width=2)
+        canvas_left.create_text(100, 40, text=f'AQI:{self.status}',font=("Helvetica",24,"bold"),fill='#9E7A7A')
+        if self.aqi<=50:
+            path="./images/green.png"
+            self.status="良好"
 
-        canvas_right=tk.Canvas(main_frame,width=200,height=200)
+        elif self.aqi<=100:
+            path="./images/yellow.png"
+            self.status="普通"
+        else:
+            path="./images/red.png"
+            self.status="危險"
+        #canvas_left.create_oval(70, 70, 130, 130, outline="#f11",fill="#1f1", width=2)
+        self.img = Image.open(path)
+        #self.resize_image = img.resize((60,60))
+        self.green = ImageTk.PhotoImage(self.img)
+        canvas_left.create_image(100, 100, anchor='center', image=self.green)      
+        canvas_left.pack(side='left')
+
+        canvas_right = tk.Canvas(main_frame,width=200,height=200)
         canvas_right.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
-        canvas_right.create_text(200,150,text="優",font=("Helvetica",24,"bold"),fill="blue")
-        canvas_right.pack(side="right")
+        canvas_right.create_text(200, 150, text=self.status,font=("Helvetica",24,"bold"),fill='#9E7A7A')
+        canvas_right.pack(side='right')
+        main_frame.pack(expand=True,fill='x')
 
-
-        main_frame.pack(expand=True,fill="x")
 
     def apply(self):
         # 當用戶按下確定時處理數據
