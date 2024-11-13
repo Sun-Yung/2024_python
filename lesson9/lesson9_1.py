665c015fcb575b417d036fc972b65613e7ae6f1d
Sun <ccps88207@gmail.com>
2024-11-13 10:12:10 +0800

    lesson9-2

--- lesson9/lesson9_1.py
+++ lesson9/lesson9_1.py
@@ -118,8 +118,7 @@ class Window(ThemedTk):
     def item_selected(self,event):
         for selected_item in self.tree.selection():
             record = self.tree.item(selected_item)
-            print(record['values'])
-            dialog=view.MycustomDialog(parent=self,title="自定義對話框")
+            dialog=view.MycustomDialog(parent=self,title="自定義對話框",record=record['values'])
 
 def main():
     datasource.download_data() #下載至資料庫
