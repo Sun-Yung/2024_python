f28dc89e5f325089560affab884a4b262d0c57ea
SUN <ccps88207@gmail.com>
2024-12-02 16:48:54 +0800

    lesson14

--- lesson14/lesson14_1.py
+++ lesson14/lesson14_1.py
@@ -1,23 +1,26 @@
 from flask import Flask,render_template
+import datasource
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return render_template("index.j2")
+    return render_template('index.j2')
 
 @app.route("/product")
 def product():
-   return render_template("product.j2")
+    cities:list[dict] = datasource.get_cities()
+    print(cities)
+    return render_template('product.j2')
 
 @app.route("/pricing")
 def pricing():
-   return render_template("pricing.j2")
+    return render_template('pricing.j2')
 
 @app.route("/faqs")
 def faqs():
-   return render_template("faqs.j2")
+    return render_template('faqs.j2')
 
 @app.route("/about")
 def about():
-   return render_template("about.j2")
+    return render_template('about.j2')
